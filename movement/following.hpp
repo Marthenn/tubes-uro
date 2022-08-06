0faccc9bd8fcb335549d8dd203544967b0997601
Marthenn <bintang.dwimarthen@gmail.com>
2022-08-06 21:19:57 +0700

    feat: menghapus fungsi yang tidak digunakan dan merubah parameter fungsi sesuai dengan implementasi

--- movement/following.hpp
+++ movement/following.hpp
@@ -1,8 +1,6 @@
 #include "objects\robot.hpp"
 #include "library\data.hpp"
 
-void putarKanan(const robot&robot);
-void putarKiri(const robot&robot);
 
-void leftFollowing(const robot&robot);
-void rightFollowing(const robot&robot);
+void leftFollowing(robot robot);
+void rightFollowing(robot robot);
