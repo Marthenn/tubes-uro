7418dbaa3abf6398b37e3e097342953deb8f87c3
Marthenn <bintang.dwimarthen@gmail.com>
2022-08-06 21:19:07 +0700

    feat: membuat algoritmanya hanya melakukan satu langkah

--- movement/implementasi/following.cpp
+++ movement/implementasi/following.cpp
@@ -1,27 +1,23 @@
 #include "movement\following.hpp"
 
+//algoritma following dengan tempel tembok kiri (hanya satu langkah)
 void leftFollowing(robot robot){
-    do{
-        if(!robot.getLeftWall()){
-            robot.putarKiri();
-        }
-        else if (robot.getLeftWall() && robot.getFrontWall()){
-            robot.putarKanan();
-        }
-        robot.maju();
+    if(!robot.getLeftWall()){
+        robot.putarKiri();
     }
-    while(!peta[robot.getLocation().second][robot.getLocation().first].getHome());
+    else if (robot.getLeftWall() && robot.getFrontWall()){
+        robot.putarKanan();
+    }
+    robot.maju();
 }
 
+//algoritma following dengan tempel tembok kanan (hanya satu langkah)
 void rightFollowing(robot robot){
-    do{
-        if(!robot.getRightWall()){
-            robot.putarKanan();
-        }
-        else if (robot.getRightWall() && robot.getFrontWall()){
-            robot.putarKiri();
-        }
-        robot.maju();
+    if(!robot.getRightWall()){
+        robot.putarKanan();
     }
-    while(!peta[robot.getLocation().second][robot.getLocation().first].getHome());
+    else if (robot.getRightWall() && robot.getFrontWall()){
+        robot.putarKiri();
+    }
+    robot.maju();
 }
